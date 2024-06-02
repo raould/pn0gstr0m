9cc2eb152d1afd0440a2333b09bbbbf73aa0c9a6
Raoul Duke <raould@gmail.com>
2024-06-01 17:35:57 -0700

    checkpoint

--- main.js
+++ main.js
@@ -17,7 +17,7 @@
 // note: the noyb2 font only has upper case letters,
 // with a few icons in the lower case.
 
-var gDebug = true;
+var gDebug = false;
 var gShowToasts = gDebug;
 
 var kCanvasName = "canvas"; // match: index.html
