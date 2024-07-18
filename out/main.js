6a477fc30c3f8c6a76080fb1df375812bad3aa40
Raoul Duke <raould@gmail.com>
2024-07-17 23:25:45 -0700

    checkpoint

--- out/main.js
+++ out/main.js
@@ -12,10 +12,10 @@ function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t =
 function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
-M; /* Copyright (C) 2011 raould@gmail.com License: GPLv2 / GNU General
-   * Public License, version 2
-   * https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
-   */
+/* Copyright (C) 2011 raould@gmail.com License: GPLv2 / GNU General
+ * Public License, version 2
+ * https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+ */
 
 // Welcome to The Land of Global Varibles, And Inconsistent Naming.
 //
