4e7023b94969d68b9ddb8d69efddbfbf9c4d6577
Raoul Duke <raould@gmail.com>
2024-09-23 07:22:28 -0700

    checkpoint

--- out/level.js
+++ out/level.js
@@ -54,8 +54,8 @@ function Level(props) {
   };
   self.IsMidGame = function () {
     var isMidGame = true;
-    if (exists(gameState.level.splitsRemaining)) {
-      isMidGame = gameState.level.splitsRemaining > 10;
+    if (exists(self.splitsRemaining)) {
+      isMidGame = self.splitsRemaining > 10;
     }
     return isMidGame;
   };
