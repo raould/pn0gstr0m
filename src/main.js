22f4ff1e0bc22e4ce0e969ba1021188f13d342cf
Raoul Duke <raould@gmail.com>
2026-07-30 18:29:24 -0700

    wip

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@
 const kIsSafari = UAParser()?.browser?.name === "Safari";
 
 // keep this committed as false.
-var gDebug = true;
+var gDebug = false;
 
 // which title menu to show?
 // true: (which is the expected shipping state) the title menu has more options.
